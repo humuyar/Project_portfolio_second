eb8c19c64f690182f62fbc86eb19cfd8bfee6462
humuyar <humiq6071@gmail.com>
2024-07-29 22:42:33 -0700

    fix the error

--- script.js
+++ script.js
@@ -1,54 +1,54 @@
 // hambergurMenu
-const hamburgerMenu = document.getElementById("hamburger-menu");
-const mobileMenu = document.getElementById("mobile-menu");
+const hamburgerMenu = document.getElementById('hamburger-menu');
+const mobileMenu = document.getElementById('mobile-menu');
 
-hamburgerMenu.addEventListener("click", () => {
-    mobileMenu.classList.toggle("hidden")
+hamburgerMenu.addEventListener('click', () => {
+  mobileMenu.classList.toggle('hidden');
 });
 // professional skill
-const professionaSskill = document.getElementById("professional-skill");
+const professionaSskill = document.getElementById('professional-skill');
 const mobileMenu1 = document.getElementById("mobile-menu1");
 
-professionaSskill.addEventListener("click", () => {
-    mobileMenu1.classList.toggle("hidden")
+professionaSskill.addEventListener('click', () => {
+  mobileMenu1.classList.toggle('hidden');
 });
 // Language
-const languages = document.getElementById("languages");
-const mobileMenu2 = document.getElementById("mobile-menu2");
+const languages = document.getElementById('languages');
+const mobileMenu2 = document.getElementById('mobile-menu2');
 
-languages.addEventListener("click", () => {
-    mobileMenu2.classList.toggle("hidden")
+languages.addEventListener('click', () => {
+  mobileMenu2.classList.toggle('hidden');
 });
 // soft skill
-const softskill = document.getElementById("soft-skill");
-const mobileMenu3 = document.getElementById("mobile-menu3");
+const softskill = document.getElementById('soft-skill');
+const mobileMenu3 = document.getElementById('mobile-menu3');
 
-softskill.addEventListener("click", () => {
-    mobileMenu3.classList.toggle("hidden")
+softskill.addEventListener('click', () => {
+  mobileMenu3.classList.toggle('hidden');
 });
 // close button for humbergurmenu
-const closebutton = document.getElementById("close-button");
-closebutton.addEventListener("click", () => {
-    mobileMenu.classList.toggle("hidden");
+const closebutton = document.getElementById('close-button');
+closebutton.addEventListener('click', () => {
+   mobileMenu.classList.toggle('hidden');
 });
-//for professional skill
-const closebutton1 = document.getElementById("close-button1");
-closebutton1.addEventListener("click", () => {
-    mobileMenu1.classList.toggle("hidden");
+// for professional skill
+const closebutton1 = document.getElementById('close-button1');
+closebutton1.addEventListener('click', () => {
+  mobileMenu1.classList.toggle('hidden');
 });
-//for language
+// for language
 const closebutton2 = document.getElementById("close-button2");
 closebutton2.addEventListener("click", () => {
-    mobileMenu2.classList.toggle("hidden");
+   mobileMenu2.classList.toggle("hidden");
 });
-//for soft skill
-const closebutton3 = document.getElementById("close-button3");
-closebutton3.addEventListener("click", () => {
-    mobileMenu3.classList.toggle("hidden");
+// for soft skill
+const closebutton3 = document.getElementById('close-button3');
+closebutton3.addEventListener('click', () => {
+  mobileMenu3.classList.toggle('hidden');
 });
-//home page  content
-const homecontent = document.getElementById("home-content");
-homecontent.innerHTML =`
+// home page  content
+const homecontent = document.getElementById('home-content');
+homecontent.innerHTML = `
 <p class="colorw txt1">
 Salaam Alikum!
 </p>
@@ -59,9 +59,9 @@ I'm Humaira Qabooli
 Glad To See You:)
 </p>
 `;
-//about page content
-const aboutcontent = document.getElementById("about-content");
-aboutcontent.innerHTML =`
+// about page content
+const aboutcontent = document.getElementById('about-content');
+aboutcontent.innerHTML = `
 <pre class="colorw">
 My name is Humaira Qabooli.
 I'm a WEB Developer.
@@ -72,7 +72,7 @@ a proffesional web page that
 will people interest with it.
 </pre>
 `;
-//certificate popup
+// certificate popup
 // const certificatepopup = document.getElementsById("certificate-popup");
 // const popupcontent = document.getElementById("popup-content");
 // const popupclose = document.getElementById("close-popup");
