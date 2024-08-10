311e7ba63b7bf9841f85643329f12a95712e3251
humuyar <humiq6071@gmail.com>
2024-08-10 01:28:47 -0700

    added some changes to style part

--- script.js
+++ script.js
@@ -80,48 +80,48 @@ const projects = [
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     image: 'assecc/WhatsApp Image 2024-08-01 at 8.09.44 AM.jpeg',
     technologies: ['html', 'css'],
-    liveLink: '#',
-    sourceLink: '#',
+    liveLink: 'https://humuyar.github.io/project-technical-documentation-page/',
+    sourceLink: 'https://github.com/humuyar/project-technical-documentation-page',
   },
   {
     title: 'palindrome checker',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     image: 'assecc/WhatsApp Image 2024-08-01 at 7.54.03 AM.jpeg',
     technologies: ['html', 'css', 'javaScript'],
-    liveLink: '#',
-    sourceLink: '#',
+    liveLink: 'https://humuyar.github.io/project_palindrome_checker/',
+    sourceLink: 'https://github.com/humuyar/project_palindrome_checker',
   },
   {
     title: 'roman numeral converter',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     image: 'assecc/WhatsApp Image 2024-08-01 at 7.54.03 AM (1).jpeg',
     technologies: ['html', 'css', 'javaScript'],
-    liveLink: '#',
-    sourceLink: '#',
+    liveLink: 'https://humuyar.github.io/roman-numeral-converter/',
+    sourceLink: 'https://github.com/humuyar/roman-numeral-converter',
   },
   {
     title: 'US number validator',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     image: 'assecc/WhatsApp Image 2024-08-01 at 7.54.04 AM.jpeg',
     technologies: ['html', 'css', 'javaScript'],
-    liveLink: '#',
-    sourceLink: '#',
+    liveLink: 'https://humuyar.github.io/Build_a_telephone_number_validator/',
+    sourceLink: 'https://github.com/humuyar/Build_a_telephone_number_validator',
   },
   {
     title: 'cash register',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     image: 'assecc/WhatsApp Image 2024-08-01 at 8.04.40 AM.jpeg',
     technologies: ['html', 'css', 'javaScript'],
-    liveLink: '#',
-    sourceLink: '#',
+    liveLink: 'https://humuyar.github.io/project_cash_register/',
+    sourceLink: 'https://github.com/humuyar/project_cash_register',
   },
   {
     title: 'pokman project',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     image: 'assecc/WhatsApp Image 2024-08-01 at 8.00.35 AM.jpeg',
     technologies: ['html', 'css', 'javaScript'],
-    liveLink: '#',
-    sourceLink: '#',
+    liveLink: 'https://humuyar.github.io/project-pokemon-search-app/',
+    sourceLink: 'https://github.com/humuyar/project-pokemon-search-app',
   },
 ];
 
@@ -129,9 +129,9 @@ const projects = [
 function createCards() {
   const container = document.getElementById('slider-container');
   projects.forEach((project, index) => {
-    const card1 = document.createElement('div');
-    card1.className = 'card1';
-    card1.innerHTML = `
+    const card = document.createElement('div');
+    card.className = 'card';
+    card.innerHTML = `
       <img src="${project.image}" alt="${project.title}">
       <h3>${project.title}</h3>
       <p>${project.description}</p>
@@ -140,16 +140,17 @@ function createCards() {
       </div>
       <button onclick="openPopup(${index})">See Project</button>
     `;
-    container.appendChild(card1);
+    container.appendChild(card);
   });
 }
 
 // Function to handle slider navigation
 let currentIndex = 0;
-
-function showSlide(index) {
+const showSlide = (index) => {
   const container = document.getElementById('slider-container');
   const totalSlides = projects.length;
+  const cardWidth = container.querySelector('.card').offsetWidth + 20;
+
   if (index >= totalSlides) {
     currentIndex = 0;
   } else if (index < 0) {
@@ -157,17 +158,17 @@ function showSlide(index) {
   } else {
     currentIndex = index;
   }
-
-  const cardWidth = container.querySelector('.card1').offsetWidth;
   container.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
-}
+};
 
 document.querySelector('.next-btn').addEventListener('click', () => showSlide(currentIndex + 1));
+document.querySelector('.next-btn').addEventListener('touchstart', () => showSlide(currentIndex + 1));
 document.querySelector('.prev-btn').addEventListener('click', () => showSlide(currentIndex - 1));
+document.querySelector('.prev-btn').addEventListener('touchstart', () => showSlide(currentIndex - 1));
 
 // Function to open popup
-function openPopup(index) {
-  const popup = document.getElementById('popup');
+window.openProjectPopup = (index) => {
+  const popup = document.getElementById('popup1');
   const popupContent = document.getElementById('popup-content-inner');
   const project = projects[index];
   popupContent.innerHTML = `
@@ -176,69 +177,82 @@ function openPopup(index) {
     <div class="technologies">
       ${project.technologies.map((tech) => `<span>${tech}</span>`).join(' ')}
     </div>
-    <a href="${project.liveLink}" target="_blank">Live Version</a>
-    <a href="${project.sourceLink}" target="_blank">Source Code</a>
+    <a href="${project.liveLink}" target="_blank" class="btn">Live Version</a>
+    <a href="${project.sourceLink}" target="_blank" class="btn">Source Code</a>
   `;
   popup.style.display = 'flex';
-}
+};
 
-document.getElementById('someButton').addEventListener('click', () => openPopup(0));
-
-// Function to close popup
 document.getElementById('close-btn').addEventListener('click', () => {
-  document.getElementById('popup').style.display = 'none';
+  document.getElementById('popup1').style.display = 'none';
 });
 
 // Initialize cards
-createCards();
-showSlide(currentIndex);
+ // Initialize cards and slider
+ createCards();
+ showSlide(currentIndex);
 
-document.addEventListener('DOMContentLoaded', () => {
-  const form = document.getElementById('contactForm');
-  const nameField = document.getElementById('name');
-  const emailField = document.getElementById('email');
-  const subjectField = document.getElementById('subject');
-  const messageField = document.getElementById('message');
-  const errorMessage = document.getElementById('errorMessage');
+ // Certificate Popup functionality
+ const toggleCertificatePopup = () => {
+   const popup = document.getElementById('myPopup');
+   popup.classList.toggle('show');
+ };
 
-  // Load data from localStorage
-  const formData = JSON.parse(localStorage.getItem('formData')) || {};
-  if (formData.name) nameField.value = formData.name;
-  if (formData.email) emailField.value = formData.email;
-  if (formData.subject) subjectField.value = formData.subject;
-  if (formData.message) messageField.value = formData.message;
+ const certificateButton = document.getElementById('open-certificate-popup');
+ const closeCertificateButton = document.getElementById('close-certificate-popup');
 
-  // Save data to localStorage on input change
-  [nameField, emailField, subjectField, messageField].forEach((field) => {
-    field.addEventListener('input', () => {
-      formData[field.id] = field.value;
-      localStorage.setItem('formData', JSON.stringify(formData));
-    });
-  });
+ certificateButton.addEventListener('click', toggleCertificatePopup);
+ certificateButton.addEventListener('touchstart', toggleCertificatePopup);
+ closeCertificateButton.addEventListener('click', toggleCertificatePopup);
+ closeCertificateButton.addEventListener('touchstart', toggleCertificatePopup);
 
-  form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    errorMessage.style.display = 'none';
+ // Form handling
+ const form = document.getElementById('contactForm');
+ const nameField = document.getElementById('name');
+ const emailField = document.getElementById('email');
+ const subjectField = document.getElementById('subject');
+ const messageField = document.getElementById('message');
+ const errorMessage = document.getElementById('errorMessage');
 
-    const name = nameField.value.trim();
-    const email = emailField.value.trim().toLowerCase();
-    const subject = subjectField.value.trim();
-    const message = messageField.value.trim();
+ // Load data from localStorage
+ const formData = JSON.parse(localStorage.getItem('formData')) || {};
+ if (formData.name) nameField.value = formData.name;
+ if (formData.email) emailField.value = formData.email;
+ if (formData.subject) subjectField.value = formData.subject;
+ if (formData.message) messageField.value = formData.message;
 
-    if (!name || !email || !subject || !message) {
-      errorMessage.textContent = 'All fields are required.';
-      errorMessage.style.display = 'block';
-      return;
-    }
+ // Save data to localStorage on input change
+ [nameField, emailField, subjectField, messageField].forEach((field) => {
+   field.addEventListener('input', () => {
+     formData[field.id] = field.value;
+     localStorage.setItem('formData', JSON.stringify(formData));
+   });
+ });
 
-    // Validate email
-    const emailPattern = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
-    if (!emailPattern.test(email)) {
-      errorMessage.textContent = 'Please enter a valid email address.';
-      errorMessage.style.display = 'block';
-      return;
-    }
-    // If validation is OK, submit the form
-    form.submit();
-  });
-});
+ form.addEventListener('submit', (event) => {
+   event.preventDefault();
+   errorMessage.style.display = 'none';
+
+   const name = nameField.value.trim();
+   const email = emailField.value.trim().toLowerCase();
+   const subject = subjectField.value.trim();
+   const message = messageField.value.trim();
+
+   if (!name || !email || !subject || !message) {
+     errorMessage.textContent = 'All fields are required.';
+     errorMessage.style.display = 'block';
+     return;
+   }
+
+   // Validate email
+   const emailPattern = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+   if (!emailPattern.test(email)) {
+     errorMessage.textContent = 'Please enter a valid email address.';
+     errorMessage.style.display = 'block';
+     return;
+   }
+
+   // If validation is OK, submit the form
+   form.submit();
+ });
+
